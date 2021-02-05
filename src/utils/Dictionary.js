7c75dde11126529df0f8fbe9a476e23d6f15ae47
Dan Koenig <dan.koenig@helloworld.com>
2021-02-04 19:04:26 -0500

    added forced function to prevent changes that place words beyond the size of the baord

--- src/utils/Dictionary.js
+++ src/utils/Dictionary.js
@@ -1,4 +1,16 @@
 import axios from 'axios';
+const bannedWords = ["anal", "anus", "arse", "ass", "ballsack", "balls", "bastard", "bitch", "biatch", "bloody", "blowjob", "bollock", "bollok", "boner", "boob", "bugger", "bum", "butt", "buttplug", "clitoris", "cock", "coon", "crap", "cunt", "damn", "dick", "dildo", "dyke", "fag", "feck", "fellate", "fellatio", "felching", "fuck", "fudgepacker", "flange", "Goddamn", "hell", "homo", "jerk", "jizz", "knobend", "labia", "lmao", "lmfao", "muff", "nigger", "nigga", "omg", "penis", "piss", "poop", "prick", "pube", "pussy", "queer", "scrotum", "sex", "shit", "sh1t", "slut", "smegma", "spunk", "tit", "tosser", "turd", "twat", "vagina", "wank", "whore", "wtf"];
+const validCharacters = ('abcdefghijklmnopqrstuvwxyz').split('');
+function isValidWord (word) {
+  const splitWord = word.toLowerCase().split('');
+  let hasInvalidCharacter = false;
+  for(let i = 0; i < splitWord.length; i++){
+    if(!validCharacters.includes(splitWord[i])){
+      hasInvalidCharacter = true;
+    }
+  }
+  return !hasInvalidCharacter && !bannedWords.includes(word.toLowerCase());
+}
 async function loadDictionary () {
     let output = {};
       await axios.get('dictionary.csv').then((result) => {
@@ -6,12 +18,11 @@ async function loadDictionary () {
         const fullList = result.data.split('\n"');
         for(let i = 1; i < fullList.length; i++){
           const word = fullList[i].split('","')[0];
-          if(word.split(' ').length == 1 && word.split('-').length == 1 && word.split("'").length == 1){
+          if(word.split(' ').length == 1 && isValidWord(word)){
             output[word.split('')[0].toUpperCase() + (i.toString())] = {word: word, defenition: fullList[i].split('","')[2]};
           }
         }
       });
-      console.log(JSON.stringify(output));
       return output;
 }
 async function loadDictionarySection (letter) {
@@ -25,13 +36,12 @@ async function loadDictionarySection (letter) {
           str = str.replace(' (', 'open-paren');
           str = str.replace(') ', 'close-paren');
           const word = str.split('open-paren')[0].split('"').join('');
-          if(word != '' && word.split(' ').length == 1 && word.split('-').length == 1 && word.split("'").length == 1){
+          if(word != '' && word.split(' ').length == 1 && isValidWord(word)){
             const def = str.split('close-paren')[1];
             output[word.split('')[0].toUpperCase() + (i.toString())] = {word: word, defenition: def};
           }
         }
       });
-      console.log(JSON.stringify(output));
       return output;
 }
 export {loadDictionary, loadDictionarySection}
